f662f86ac698fe1319f43b7d80bdd11801082d9a
tstagge <tystagge@outlook.com>
2017-03-04 03:34:16 -0500

    (03/04: TS) NEW  BasicMovement.c, Gyro.c; UPDATED: driver

---
+++ BasicMovement.c
@@ -0,0 +1,73 @@
+//ENGR 142: Project 3
+//Team 57
+//  Jillian Hestle, Emily Schott, Nicholas Vilbrandt, Tyler Stagge
+
+//BASIC MOBILITY and OPERATION FUNCTIONS [BasicMovement.c]
+//
+// This file contains all the functions the robot will use to move or otherwise
+// actuate any of its components. See [Driver.c] for all pragmas.
+
+//FIXME: Need to figure out how to properly refer to motors by their assigned
+// names as opposed to "motorA" and "motorB"
+
+/*----------------------------FUNCTION  PROTOTYPES----------------------------*/
+void halt(); //Stops drive motors only
+void fullStop(); //Stops all motors
+void drive(int power, int timeMS);
+void driveIndef(int power);
+void pointTurn(int power, int timeMS); //Positive is LEFT
+void pointTurnIndef(int power);
+void pivotTurn(int power, int timeMS); //Positive is LEFT
+
+/*----------------------------FUNCTION DEFINITIONS----------------------------*/
+void halt()
+{
+	motor[motorA] = 0;
+	motor[motorB] = 0;
+	//sleep required?
+}
+
+void fullStop()
+{
+	motor[motorA] = 0;
+	motor[motorB] = 0;
+	motor[motorC] = 0;
+}
+
+void driveIndef(int power)
+{
+	motor[motorA] = power;
+	motor[motorB] = power;
+}
+
+void drive(int power, int timeMS)
+{
+	motor[motorA] = power;
+	motor[motorB] = power;
+	sleep(timeMS);
+	halt();
+}
+
+void pointTurn(int power, int timeMS)
+{
+	motor[motorA] = -power;
+	motor[motorB] = power;
+	sleep(timeMS);
+	halt();
+}
+
+void pointTurnIndef(int power)
+{
+	motor[motorA] = -power;
+	motor[motorB] = power;
+}
+
+void pivotTurn(int power, int timeMS)
+{
+	if(power > 0)    //If left
+		motor[motorB]; //power right
+	else             //If right
+		motor[motorA]; //power left
+	sleep(timeMS)
+	halt();
+}
--- Driver.c
+++ Driver.c
@@ -3,14 +3,38 @@
 //  Jillian Hestle, Emily Schott, Nicholas Vilbrandt, Tyler Stagge
 
 //PRIMARY DRIVER [Driver.c]
+//
+// This file contains the main function from which all other functions are
+// called, in addition to the robot's complete sweep of pragmas
 
 /*----------------------------------PRAGMAS-----------------------------------*/
+#pragma config(Motor,  motorA,          motorLeft,     tmotorNXT, PIDControl, encoder)
+#pragma config(Motor,  motorB,          motorRight,    tmotorNXT, PIDControl, encoder)
+#pragma config(Motor,  motorC,          motorDep,      tmotorNXT, PIDControl, encoder)
 
+#include "BasicMovement.c"
 
 /*----------------------------FUNCTION  PROTOTYPES----------------------------*/
-
+void runCOTTest(); //Code for a center-of-turning test
 
 /*-----------------------------------MAIN-------------------------------------*/
-
+task main()
+{
+	runCOTTest();
+}
 
 /*----------------------------FUNCTION DEFINITIONS----------------------------*/
+void runCOTTest()
+{
+	int numTurns = 4;
+	int turnPower = 50;
+
+	int i = 0;
+	for(i = 0; i < numTurns; i++)
+	{
+		pointTurn(turnPower,1600);
+		sleep(300);
+		pointTurn(-turnPower,1600);
+		sleep(300);
+	}
+}
