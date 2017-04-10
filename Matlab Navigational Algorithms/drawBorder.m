c151ad2a47e9d41e2a64d5e18c80e2864c7154bb
tstagge <tystagge@outlook.com>
2017-04-10 03:17:18 -0400

    PathGenerator is a working in progress
    Currently working on the binary matrix permutation generator that will
    power the path generator

---
+++ Matlab Navigational Algorithms/drawBorder.m
@@ -0,0 +1,14 @@
+function void = drawBorder(size)
+    x = [0,size(2)];
+    y = [0,-size(1)];
+    
+    top = [x(1), x(2); y(1), y(1)];
+    bottom = [x(1), x(2); y(2), y(2)];
+    left = [x(1), x(1); y(1), y(2)];
+    right = [x(2), x(2); y(1), y(2)];
+    
+    plot(top(1,:), top(2,:), 'k');
+    plot(bottom(1,:), bottom(2,:), 'k');
+    plot(left(1,:), left(2,:), 'k');
+    plot(right(1,:), right(2,:), 'k');
+end
---
+++ Matlab Navigational Algorithms/getBinaryMatrix.m
@@ -0,0 +1,6 @@
+%IN: n = number of 'blocks' (number of nodes - 1)
+%OUT: binMat = every permutation of a 2 by 2n binary matrix
+
+function binMat = getBinaryMatrix(n)
+
+end
